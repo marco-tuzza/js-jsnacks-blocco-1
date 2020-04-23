868977e69456e95a728a73d5bceef80d8e220516
Marco Tuzza <marcotuzza@hotmail.it>
2020-04-23 20:25:16 +0200

    jsnack 6 terminato

---
+++ jsnack6/main.js
@@ -0,0 +1,11 @@
+// Stampa le potenze di 2 fino a 1000
+
+// Creiamo una variabile che ci faccia da contenitore
+var potenza
+// Creiamo un ciclo di 8 elementi (se lo facessimo di 9 sforeremmo il limite di 1000 per il risultato)
+for (var i = 0; i <= 8; i++) {
+    // Utiliziamo la variabile potenza e ogni ciclo moltiplichiamo per 2
+    potenza = potenza * 2
+    // Stampiamo i risultati
+    console.log('2 elevato alla ' + (i+1) + ' = ' + potenza);
+}
